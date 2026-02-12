ae87c2ded03657674f18cdef0fabd01ea5afd3b8
nainJaramillo <aratenorio6@gmail.com>
2026-02-12 11:33:28 +0100

    Ficheros-67-Nain

--- src/main/java/es/fplumara/dam1/config/AppConfig.java
+++ src/main/java/es/fplumara/dam1/config/AppConfig.java
@@ -1,16 +1,28 @@
 package es.fplumara.dam1.config;
+import es.fplumara.dam1.exceptions.ConfigException;
 
-
+import java.io.InputStream;
+import java.nio.file.*;
 import java.util.Properties;
 
-public class AppConfig {
+public class AppConfig{
     private String storeType;
     private String messagesFile;
     private int messagesMaxLength;
 
-     public AppConfig(){
-         Properties props= new Properties();
+    public AppConfig(){
+            try {
+                Properties props = new Properties();
+                Path path = Path.of("config.properties");
+                InputStream in = Files.newInputStream(path);
+                props.load(in);
+                storeType  = props.getProperty("store.type");
+                messagesFile = props.getProperty("messages.file");
+                messagesMaxLength = Integer.parseInt(props.getProperty("messages.maxLength"));
+            }catch(Exception ex){
+                throw new ConfigException("No se puede encontrar el archivo data/config.properties");
 
-     }
+            }
+        }
 
 }
